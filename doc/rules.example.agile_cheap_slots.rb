ee19c399c6ba71e7e69ee3c574fb9be6cd0a3afe
Chris Elsworth <chris@cae.me.uk>
2020-02-12 11:43:03 +0000

    rework discharging logic

--- doc/rules.example.agile_cheap_slots.rb
+++ doc/rules.example.agile_cheap_slots.rb
@@ -16,7 +16,7 @@
 # to integrate Solcast data to try and determine exactly which days will be sunny,
 # automatically.
 #
-# It charges if the price is 1p or less, even if the SOC is high. It also
+# It charges if the price is less than 2p, even if the SOC is high. It also
 # enables charging for an "emergency boost" if it detects a peak period is
 # approaching (defined as over 15p) and your SOC is below 50%.
 #
@@ -50,14 +50,15 @@ LOGGER.info "SOC = #{soc}% / #{required_soc}%, " \
   "hours = #{hours_required.round(2)}"
 
 # cheap_slot_data.json is our cache of what we'll be doing tonight.
-# the root has two keys; slots and updated_at.
-# updated_at is set to the current time when we update it, then in subsequent runs
-# we know we have current data until tomorrow.
 f = Pathname.new('cheap_slot_data.json')
 data = f.readable? ? JSON.parse(f.read) : {}
 updated_at = data['updated_at']
 
-if Time.now.hour >= 21 && (updated_at.nil? || Time.now - Time.parse(updated_at) > 14_400)
+after_8 = Time.now.hour >= 20
+# stale = updated_at.nil? || Time.now - Time.parse(updated_at) > 14_400
+have_prices = octopus.prices.count > 20
+
+if after_8 && have_prices
   # if it is later than 9pm and we haven't run today, do so now
   cheapest_slots = octopus.prices
                           .take(20) # 10 hours
@@ -65,7 +66,7 @@ if Time.now.hour >= 21 && (updated_at.nil? || Time.now - Time.parse(updated_at) 
                           .take(slots_required)
                           .sort.to_h
 
-  cheapest_slots.each { |time, price| puts "#{time} #{price}p" }
+  cheapest_slots.each { |time, price| LOGGER.debug "Charge Slot: #{time} at #{price}p" }
 
   data = { 'updated_at' => Time.now, 'slots' => cheapest_slots }
   f.write(JSON.generate(data))
@@ -78,11 +79,11 @@ charge = data['slots'].any? do |time, _price|
   time == now
 end
 
-LOGGER.info 'Charging due cheap_slot_data' if charge
+LOGGER.info 'Charging due to cheap_slot_data' if charge
 
 # override for any really cheap energy as a failsafe
-if octopus.price <= 1
-  LOGGER.info 'Charging due to price <= 1p'
+if octopus.price < 2
+  LOGGER.info 'Charging due to price < 2p'
   charge = true
 end
 
@@ -92,33 +93,18 @@ if soc < 50 && octopus.prices.values.take(3).max > 15 && octopus.price < 15
   charge = true
 end
 
-# depending on how much SOC we have, energy that is "cheap enough" can
-# save discharge for a more expensive period.
-discharge_pct = 100 # default to discharging
-if octopus.prices.count > 6 # don't do this if we don't have 3 hours of future data
-  # consider the next 10 hours
-  sorted_by_price = octopus.prices.take(20).sort_by { |_k, v| v }.to_h.values
+# do not discharge if the electricity price is a percentage within the cost
+# we've charged at. this avoids charging at 1p, to then discharge at 1.1p an hour later.
+max_charge_price = data['slots'].values.max || 0
+discharge_price_cap = max_charge_price * 1.2
+discharge_pct = 100
+discharge_pct = 0 if soc < 50 && octopus.price <= discharge_price_cap
 
-  c = sorted_by_price.count
-  cheapest_10pct = sorted_by_price.take(c * 0.1).last
-  cheapest_20pct = sorted_by_price.take(c * 0.2).last
-  cheapest_50pct = sorted_by_price.take(c * 0.5).last
+LOGGER.debug "max_charge_price = #{max_charge_price}p; discharge_price_cap = #{discharge_price_cap}p"
 
-  LOGGER.info "Discharge cutoffs: 30%SOC = #{cheapest_50pct}p, " \
-    "40%SOC = #{cheapest_20pct}p, 50%SOC = #{cheapest_10pct}p"
-
-  if soc < 30 && octopus.price <= cheapest_50pct
-    LOGGER.info 'Idle to use cheapest 50% price'
-    discharge_pct = 0
-  elsif soc < 40 && octopus.price <= cheapest_20pct
-    LOGGER.info 'Idle to use cheapest 20% price'
-    discharge_pct = 0
-  elsif soc < 50 && octopus.price <= cheapest_10pct
-    LOGGER.info 'Idle to use cheapest 10% price'
-    discharge_pct = 0
-  end
+if lc.discharge_pct != discharge_pct
+  r = (lc.discharge_pct = discharge_pct)
+  exit 255 unless r == discharge_pct
 end
 
-lc.discharge_pct = discharge_pct if lc.discharge_pct != discharge_pct
-
-lc.charge(charge)
+exit 255 unless lc.charge(charge)
