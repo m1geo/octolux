2dc9610a0c0cd6a6d3581390fedb1d4e186277f6
Chris Elsworth <chris@cae.me.uk>
2020-02-22 19:26:53 +0000

    Add shellscript wrapper

---
+++ octolux.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Simple wrapper around octolux.rb
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+run() {
+  $DIR/octolux.rb >>$DIR/octolux.log
+}
+
+# If octolux.rb quits with a non-zero status code, run it again.
+# This catches occasional failures due to the inverter not responding etc.
+run || run
