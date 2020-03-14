4beb9767b9a1ac9c3ab38e8934d2af634652ae6f
Chris Elsworth <chris@cae.me.uk>
2020-03-14 20:08:36 +0000

    rewrite rules.example.agile_cheap_slots.rb

--- doc/rules.example.agile_cheap_slots.rb
+++ doc/rules.example.agile_cheap_slots.rb
@@ -1,104 +1,98 @@
 # frozen_string_literal: true
 
-# this rules file attempts to intelligently set charging periods overnight,
-# using the cheapest energy possible with Agile half-hourly prices. it attempts
-# to get your batteries up to the "required_soc" percentage. This is worked out
-# on the first run after 9pm (so we have overnight Agile prices).
-#
-# This works best if the script knows your current SOC, which means server.rb
-# needs to be accessible. If it's not, this will assume the batteries are empty.
-#
-# TODO: the required_soc is currently hardcoded. If you have solar panels, this
-# should be high in winter (90-100%) as there will be no sun, but lower in summer,
-# as otherwise you may charge your batteries using energy costing money, but then
-# the sun comes out and you could have charged them for free. So lowering it in
-# summer will avoid using too much imported energy. A future improvement here is
-# to integrate Solcast data to try and determine exactly which days will be sunny,
-# automatically.
-#
-# It charges if the price is less than 2p, even if the SOC is high. It also
-# enables charging for an "emergency boost" if it detects a peak period is
-# approaching (defined as over 15p) and your SOC is below 50%.
-#
-# Additionally, it turns off discharging (making the inverter idle) if the
-# Agile price is "cheap enough". This is a bit rudimentary at the moment but
-# is currently set to activate if you're below 50% SOC, and the price is less
-# than 1.2x the highest price we have in the cheap_slots file.
-
 LOGGER.info "Current Price = #{octopus.price}p"
 
-# constants that could move to config
-battery_count = 6
-charge_rate = 3.3 # kW
-
-# TODO: solcast forecast could bias this
 required_soc = CONFIG['rules']['required_soc'].to_i
-soc = ls.inputs['soc'] || 10 # assume 10% if we don't have it
 
-system_size = 2.4 * battery_count # kWh per battery * number of batteries
-charge_size = system_size * ((required_soc - soc) / 100.0)
-hours_required = charge_size / charge_rate # at a charge rate of 3.3kW
-slots_required = (hours_required * 2).ceil # half-hourly Agile periods
+if (soc = ls.inputs['soc'])
+  LOGGER.info "SOC = #{soc}% / #{required_soc}%"
+else
+  LOGGER.warn "Can't get SOC % from server.rb, assuming 10%"
+  soc = 10
+end
 
-slots_required = 0 if slots_required.negative?
+slots = octopus.prices.sort_by { |_k, v| v }
 
-LOGGER.info "SOC = #{soc}% / #{required_soc}%, " \
-  "charge_size = #{charge_size.round(2)} kWh, " \
-  "hours = #{hours_required.round(2)}"
+now = Time.at(1800 * (Time.now.to_i / 1800))
 
-# cheap_slot_data.json is our cache of what we'll be doing tonight.
 f = Pathname.new('cheap_slot_data.json')
 data = f.readable? ? JSON.parse(f.read) : {}
 
-after_8 = Time.now.hour >= 20 || Time.now.hour < 1
-# stale = data['updated_at'].nil? || Time.now - Time.parse(data['updated_at']) > 14_400
+# charge_slots are worked out each evening after 9pm, when we have new Octopus price data.
+evening = now.hour >= 21
+stale = data['updated_at'].nil? || Time.now - Time.parse(data['updated_at']) > (23 * 60 * 60)
 have_prices = octopus.prices.count > 20
+if (evening || stale) && have_prices
+  battery_count = CONFIG['lxp']['batteries'].to_i
+  charge_rate = [3, battery_count].min
 
-if after_8 && have_prices
-  cheapest_slots = octopus.prices
-                          .take(20) # 10 hours
-                          .sort_by { |_k, v| v }.to_h
-                          .take(slots_required)
-                          .sort.to_h
+  system_size = 2.4 * battery_count # kWh per battery
+  # TODO: bias this based on solcast?
+  charge_size = system_size * ((required_soc - soc) / 100.0)
+  charge_size = 0 if charge_size.negative?
+  hours_required = charge_size / charge_rate.to_f
+  slots_required = (hours_required * 2).ceil # half-hourly Agile periods
 
-  cheapest_slots.each { |time, price| LOGGER.info "Charge Slot: #{time} at #{price}p" }
+  LOGGER.info "charge_size = #{charge_size.round(2)} kWh, hours = #{hours_required.round(2)}"
 
-  data = { 'updated_at' => Time.now, 'slots' => cheapest_slots }
+  # the cheapest slots are used to charge up to required_soc. these are restricted to 10pm-8am
+  # so we don't end up trying to charge during the day in normal circumstances.
+  night_slots = slots.select do |time, _price|
+    (now.hour > 8 && now.day == time.day && time.hour >= 22) || time.hour <= 8
+  end
+  charge_slots = night_slots.shift(slots_required).to_h
+
+  data = { 'updated_at' => Time.now, 'charge_slots' => charge_slots }
+
   f.write(JSON.generate(data))
+else
+  charge_slots = data['charge_slots'] || {}
+
+  # convert keys back to Time objects
+  charge_slots = charge_slots.map { |time, price| [Time.parse(time), price] }.to_h
 end
 
-# enable charge if any of the keys in data['slots'] match the current half-hour period
-now = Time.at(1800 * (Time.now.to_i / 1800))
-charge = data['slots'].any? do |time, _price|
-  time = Time.parse(time) unless time.is_a?(Time)
-  time == now
-end
+# merge in any slots with a price cheaper than cheap_charge from config
+cheap_charge = CONFIG['rules']['cheap_charge'].to_f
+cheap_slots = octopus.prices.select { |_time, price| price < cheap_charge }
+charge_slots.merge!(cheap_slots)
 
-LOGGER.info 'Charging due to scheduled charge slot' if charge
+charge_slots.sort.each { |time, price| LOGGER.info "Charge: #{time} @ #{price}p" }
 
-# override for any really cheap energy as a failsafe
-if octopus.price < 2
-  LOGGER.info 'Charging due to really cheap price'
-  charge = true
-end
+max_charge_price = charge_slots[charge_slots.keys.last] || cheap_charge
+# discharge_slots depends how much SOC we have. The more SOC, we more generous we can be
+# with discharging. The lower the SOC, the more we want to save that charge for higher prices.
+multiplier = if soc < 30 then 1.6
+             elsif soc < 40 then 1.5
+             elsif soc < 50 then 1.4
+             elsif soc < 60 then 1.3
+             elsif soc < 80 then 1.2
+             else
+               1.1
+             end
+min_discharge_price = (max_charge_price * multiplier).round(4)
+discharge_slots = slots.delete_if { |_k, v| v <= min_discharge_price }.to_h
+# discharge_slots.sort.each { |time, price| LOGGER.info "Discharge: #{time} @ #{price}p" }
+
+# this is just used for informational logging. Need to convert back to Time if loaded from JSON.
+t = (charge_slots.keys + discharge_slots.keys).map { |n| n.is_a?(Time) ? n : Time.parse(n) }
+idle = octopus.prices.reject { |k, _v| t.include?(k) }
+idle.sort.each { |time, price| LOGGER.info "Idle: #{time} @ #{price}p" }
+
+# enable ac_charge/discharge if any of the keys match the current half-hour period
+ac_charge = charge_slots.any? { |time, _price| time == now }
+discharge = discharge_slots.any? { |time, _price| time == now }
+
+LOGGER.info "ac_charge = #{ac_charge} ; discharge = #{discharge} (> #{min_discharge_price}p)"
 
 # if a peak period is approaching and we're under 50%, start emergency charge
 if soc < 50 && octopus.prices.values.take(3).max > 15 && octopus.price < 15
   LOGGER.warn 'Peak approaching, emergency charging'
-  charge = true
+  ac_charge = true
 end
 
-# do not discharge if the electricity price is not much higher than we've charged at.
-# this avoids charging at 2p, to then discharge at 2.1p an hour later, which would be
-# a loss after charging overheads
-avg_charge_price = (data['slots'].values.inject(:+) / data['slots'].count) || 0
-discharge_price_cap = (avg_charge_price * 1.2) + 1
-discharge_pct = octopus.price <= discharge_price_cap ? 0 : 100
-
-LOGGER.info "avg_charge_price = #{avg_charge_price.round(3)}p; " \
-  "discharge_price_cap = #{discharge_price_cap.round(3)}p"
-
+discharge_pct = discharge ? 100 : 0
 r = (lc.discharge_pct = discharge_pct)
 exit 255 unless r == discharge_pct
 
-exit 255 unless lc.charge(charge)
+exit 255 unless lc.charge(ac_charge)
